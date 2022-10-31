e4868d96f4c80b00e1c27927124be17e889a7c86
Basfaj <basfaj80@gmail.com>
2022-10-31 15:52:53 +0100

    setting of work directory and loading of reqiured packages for Data visualization in Rstudio.

---
+++ Data visualisation to Github.R
@@ -0,0 +1,17 @@
+# Firstly setting of the working directory by using function "setwd" or 
+# manually by running code "file.choose()"
+setwd("C:/Users/USER/Desktop/FNABHABAF/DATA science/R/R_Praticals")
+getwd()
+
+#installing and loading of packages needed
+install.packages("readr")
+install.packages("readxl")
+install.packages("dplyr")
+install.packages("tidyverse")
+install.packages("ggplot2")
+#load package
+library(readr)
+library(readxl)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
